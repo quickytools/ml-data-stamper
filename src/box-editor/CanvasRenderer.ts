0c506f62dcc19bae6f773e41d957354991dedd20
Romii646 <cortinacort@gmail.com>
2025-06-08 11:41:32 -0400

    this commit will add pinia library to main.ts. BoundingBoxEditor file has been refactored with methods and variables grouped into two classes selectionArea and CanvasRenderer.

---
+++ src/box-editor/CanvasRenderer.ts
@@ -0,0 +1,206 @@
+import { ref } from 'vue'
+import { useVideoStore } from '../stores/videoStore'
+import { SelectionArea } from '../box-editor/SelectionArea'
+
+export class CanvasRenderer {
+  private getFrame: () => HTMLImageElement | null // from videoStore
+  private editorCanvas = ref()
+  private editorScale = ref()
+  private interactionCursor = ref()
+  private panState
+  private selectionArea = new SelectionArea()
+
+  constructor(
+    editorCanvas: HTMLCanvasElement,
+    getFrame: () => HTMLImageElement | null,
+    editorScale: number,
+    panState,
+    selectionArea: SelectionArea,
+    interactionCursor: object,
+  ) {
+    this.editorCanvas.value = editorCanvas
+    this.getFrame = getFrame
+    this.editorScale.value = editorScale
+    this.panState = panState
+    this.selectionArea = selectionArea
+    this.interactionCursor.value = interactionCursor
+  }
+
+  private drawCheckerboard(canvas, { startX, startY, width, height, lightColor, darkColor }) {
+    const ctx = canvas.getContext('2d')
+    const x0 = Math.round(startX * 0.1) * 10
+    const y0 = Math.round(startY * 0.1) * 10
+    for (let i = 0; i <= height / 10; i++) {
+      for (let j = 0; j <= width / 10; j++) {
+        const x = x0 + j * 10
+        const y = y0 + i * 10
+        ctx.fillStyle = ((x + y) * 0.1) % 2 ? lightColor : darkColor
+        ctx.fillRect(x, y, 10, 10)
+      }
+    }
+  }
+
+  canvasBackground = () => {
+    const canvas = this.editorCanvas.value
+    const ctx = canvas.getContext('2d')
+    const { a, e, f } = ctx.getTransform()
+
+    const inverseScale = a > 0 ? 1 / a : 1
+    const canvasWidth = canvas.width
+    const canvasHeight = canvas.height
+    const scaledHeight = canvasHeight * inverseScale
+    const scaledWidth = canvasWidth * inverseScale
+    const startX = -e * inverseScale
+    const startY = -f * inverseScale
+
+    ctx.clearRect(startX, startY, scaledWidth, scaledHeight)
+
+    const frame = this.getFrame()
+    if (frame) {
+      ctx.drawImage(frame, scaledWidth, scaledHeight) // arg 1. for elements like video, canvas, and images, 2nd & 3rd args are x and y coordinates
+    } else {
+      this.drawCheckerboard(canvas, {
+        startX,
+        startY,
+        width: scaledWidth,
+        height: scaledHeight,
+        lightColor: 'rgba(255, 255, 255, 0.80)',
+        darkColor: 'rgba(0, 0, 0, 0.05)',
+      })
+    }
+  }
+
+  zoom = (mouseCoordinate: { x: number; y: number }, deltaY: number) => {
+    const canvas = this.editorCanvas.value
+    const ctx = canvas.getContext('2d')
+
+    const xform = ctx.getTransform()
+
+    const prevScale = xform.a
+
+    let newScale = prevScale + deltaY * -0.01 // Adjust the zoom speed as needed
+    newScale = Math.min(Math.max(0.5, newScale), 10)
+
+    const scaleChange = newScale / prevScale
+
+    const { x, y } = mouseCoordinate
+    const updatedXform = new DOMMatrix()
+      .translate(x, y)
+      .scale(scaleChange)
+      .translate(-x, -y)
+      .multiply(xform)
+
+    this.editorScale = newScale
+
+    ctx.setTransform(updatedXform)
+
+    this.canvasBackground() // redraws the background
+    this.paintIt(this.selectionArea)
+  }
+
+  moveCanvasView = (action: MouseEvent) => {
+    const currentCoordinates = { x: action.clientX, y: action.clientY }
+    const { x, y } = this.panState.onMove(currentCoordinates)
+    const ctx = this.editorCanvas.value.getContext('2d')
+    const { a } = ctx.getTransform()
+    ctx.setTransform(a, 0, 0, a, x, y)
+
+    this.canvasBackground()
+    this.paintIt(this.selectionArea)
+  }
+
+  drawOnCanvas = (x1: number, y1: number, x2: number, y2: number) => {
+    const canvas = this.editorCanvas.value
+    const ctx = canvas.getContext('2d')
+
+    this.canvasBackground()
+    this.interactionCursor.value.crosshair = true
+
+    const startX = Math.min(x1, x2)
+    const startY = Math.min(y1, y2)
+    const width = Math.abs(x2 - x1)
+    const height = Math.abs(y2 - y1)
+
+    this.selectionArea.x = startX
+    this.selectionArea.y = startY
+    this.selectionArea.width = width
+    this.selectionArea.height = height
+
+    this.paintIt(this.selectionArea)
+  }
+
+  // function to move the rectangle
+  movingRectangle = (x: number, y: number) => {
+    const Canvas = this.editorCanvas.value
+    const ctx = Canvas.getContext('2d')
+
+    this.canvasBackground()
+
+    this.selectionArea.move(x, y)
+
+    this.paintIt(this.selectionArea)
+  }
+
+  // model function to resize the rectangle
+  resizingRectangle = (x: number, y: number) => {
+    const canvas = this.editorCanvas.value
+    const ctx = canvas.getContext('2d')
+
+    this.canvasBackground()
+
+    // Handle each side resizing
+    if (this.interactionCursor.value.sizingDirection.left) {
+      const newWidth = this.selectionArea.x + this.selectionArea.width - x
+      if (newWidth >= this.selectionArea.outerLayer) {
+        this.selectionArea.x = x
+        this.selectionArea.width = newWidth
+      }
+    }
+    if (this.interactionCursor.value.sizingDirection.right) {
+      const newWidth = x - this.selectionArea.x
+      if (newWidth >= this.selectionArea.outerLayer) {
+        this.selectionArea.width = newWidth
+      }
+    }
+    if (this.interactionCursor.value.sizingDirection.top) {
+      const newHeight = this.selectionArea.y + this.selectionArea.height - y
+      if (newHeight >= this.selectionArea.outerLayer) {
+        this.selectionArea.y = y
+        this.selectionArea.height = newHeight
+      }
+    }
+    if (this.interactionCursor.value.sizingDirection.bottom) {
+      const newHeight = y - this.selectionArea.y
+      if (newHeight >= this.selectionArea.outerLayer) {
+        this.selectionArea.height = newHeight
+      }
+    }
+
+    this.paintIt(this.selectionArea)
+  }
+
+  // view function for user to see the rectangle
+  paintIt = (area: { x: number; y: number; width: number; height: number }) => {
+    const canvas = this.editorCanvas.value
+    const ctx = canvas.getContext('2d')
+    ctx.beginPath()
+    ctx.fillStyle = 'rgba(255, 0, 0, 0.05)'
+    ctx.fillRect(area.x, area.y, area.width, area.height)
+  }
+
+  getMousePositionOnCanvas(action: MouseEvent) {
+    const canvas = this.editorCanvas.value
+    const ctx = canvas.getContext('2d')
+
+    const x = action.offsetX
+    const y = action.offsetY
+
+    const transform = ctx.getTransform()
+    const transformedPoint = new DOMPoint(x, y).matrixTransform(transform.inverse())
+
+    return {
+      x: transformedPoint.x,
+      y: transformedPoint.y,
+    }
+  }
+}
--- src/components/BoundingBoxEditor.vue
+++ src/components/BoundingBoxEditor.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
-import { ref, onMounted, nextTick, watch } from 'vue'
+import { ref, onMounted, nextTick } from 'vue'
 import { SelectionArea } from '../box-editor/SelectionArea'
 import { useVideoStore } from '../stores/videoStore'
+import { CanvasRenderer } from '../box-editor/CanvasRenderer'
 
 const editorCanvas = ref()
 const editorCanvasWidth = ref(0)
 const editorCanvasHeight = ref(0)
-const editorScale = ref(1) // this is the scale factor for the canvas, used for zooming in and out
-const videoStore = useVideoStore() // stored videoFrames
+const editorScale = ref(1) //  scale factor for the canvas, used for zooming in and out
+const videoStore = useVideoStore() // stored videoFrames use videoStore.currentFrame
 const canvasInteractionState = ref({
   isDrawing: false,
   isDraggable: false,
   isResizing: false,
   isPanning: false,
-}) // this is the controller object that will be used to control the annotation box
+})
 const interactionCursor = ref({
   sizingDirection: { left: false, right: false, top: false, bottom: false },
   hovering: false,
@@ -37,186 +38,11 @@ const panState = {
   },
 }
 const mouseCanvasCoordinate = { x: 0, y: 0 } // this is the coordinate object that will be used to store the mouse position on the canvas
-const selectionArea = new SelectionArea() //annotation box
-
-function drawCheckerboard(canvas, { startX, startY, width, height, lightColor, darkColor }) {
-  const ctx = canvas.getContext('2d')
-  const x0 = Math.round(startX * 0.1) * 10
-  const y0 = Math.round(startY * 0.1) * 10
-  for (let i = 0; i <= height / 10; i++) {
-    for (let j = 0; j <= width / 10; j++) {
-      const x = x0 + j * 10
-      const y = y0 + i * 10
-      ctx.fillStyle = ((x + y) * 0.1) % 2 ? lightColor : darkColor
-      ctx.fillRect(x, y, 10, 10)
-    }
-  }
-}
-
-const canvasBackground = () => {
-  const canvas = editorCanvas.value
-  const ctx = canvas.getContext('2d')
-  const { a, e, f } = ctx.getTransform()
-
-  const inverseScale = a > 0 ? 1 / a : 1
-  const canvasWidth = canvas.width
-  const canvasHeight = canvas.height
-  const scaledHeight = canvasHeight * inverseScale
-  const scaledWidth = canvasWidth * inverseScale
-  const startX = -e * inverseScale
-  const startY = -f * inverseScale
-
-  ctx.clearRect(startX, startY, scaledWidth, scaledHeight)
-
-  drawCheckerboard(canvas, {
-    startX,
-    startY,
-    width: scaledWidth,
-    height: scaledHeight,
-    lightColor: 'rgba(255, 255, 255, 0.80)',
-    darkColor: 'rgba(0, 0, 0, 0.05)',
-  })
-}
-
-const zoom = (mouseCoordinate: { x: number; y: number }, deltaY: number) => {
-  const canvas = editorCanvas.value
-  const ctx = canvas.getContext('2d')
-
-  const xform = ctx.getTransform()
-
-  const prevScale = xform.a
-
-  let newScale = prevScale + deltaY * -0.01 // Adjust the zoom speed as needed
-  newScale = Math.min(Math.max(0.5, newScale), 10)
-
-  const scaleChange = newScale / prevScale
-
-  const { x, y } = mouseCoordinate
-  const updatedXform = new DOMMatrix()
-    .translate(x, y)
-    .scale(scaleChange)
-    .translate(-x, -y)
-    .multiply(xform)
-
-  editorScale.value = newScale
-
-  ctx.setTransform(updatedXform)
-
-  canvasBackground() // redraws the background
-  paintIt(selectionArea) // redraw the rectangle
-}
-
-const moveCanvasView = (action: MouseEvent) => {
-  const currentCoordinates = { x: action.clientX, y: action.clientY }
-  const { x, y } = panState.onMove(currentCoordinates)
-  const ctx = editorCanvas.value.getContext('2d')
-  const { a } = ctx.getTransform()
-  ctx.setTransform(a, 0, 0, a, x, y)
-
-  canvasBackground()
-  paintIt(selectionArea)
-}
-
-const drawOnCanvas = (x1: number, y1: number, x2: number, y2: number) => {
-  const canvas = editorCanvas.value
-  const ctx = canvas.getContext('2d')
-
-  canvasBackground() // redraws the background
-  interactionCursor.value.crosshair = true // this is used to show the crosshair cursor when the user is drawing
-
-  const startX = Math.min(x1, x2)
-  const startY = Math.min(y1, y2)
-  const width = Math.abs(x2 - x1)
-  const height = Math.abs(y2 - y1)
-
-  // storing the rectangle coordinates and size in the rectangle object
-  selectionArea.x = startX
-  selectionArea.y = startY
-  selectionArea.width = width
-  selectionArea.height = height
-
-  paintIt(selectionArea) // this function is used to draw the rectangle on the canvas
-}
-
-// model function to move the rectangle
-const movingRectangle = (x: number, y: number) => {
-  const Canvas = editorCanvas.value
-  const ctx = Canvas.getContext('2d')
-
-  canvasBackground() //redraws the background
-
-  // updating the rectangle object with the new coordinates and size with the mouse click
-  selectionArea.move(x, y)
-
-  paintIt(selectionArea)
-}
-
-// model function to resize the rectangle
-const resizingRectangle = (x: number, y: number) => {
-  const canvas = editorCanvas.value
-  const ctx = canvas.getContext('2d')
-
-  canvasBackground()
-
-  // Handle each side resizing
-  if (interactionCursor.value.sizingDirection.left) {
-    const newWidth = selectionArea.x + selectionArea.width - x
-    if (newWidth >= selectionArea.outerLayer) {
-      selectionArea.x = x
-      selectionArea.width = newWidth
-    }
-  }
-  if (interactionCursor.value.sizingDirection.right) {
-    const newWidth = x - selectionArea.x
-    if (newWidth >= selectionArea.outerLayer) {
-      selectionArea.width = newWidth
-    }
-  }
-  if (interactionCursor.value.sizingDirection.top) {
-    const newHeight = selectionArea.y + selectionArea.height - y
-    if (newHeight >= selectionArea.outerLayer) {
-      selectionArea.y = y
-      selectionArea.height = newHeight
-    }
-  }
-  if (interactionCursor.value.sizingDirection.bottom) {
-    const newHeight = y - selectionArea.y
-    if (newHeight >= selectionArea.outerLayer) {
-      selectionArea.height = newHeight
-    }
-  }
-
-  paintIt(selectionArea)
-}
-
-// view function for user to see the rectangle
-const paintIt = (area: { x: number; y: number; width: number; height: number }) => {
-  const canvas = editorCanvas.value
-  const ctx = canvas.getContext('2d')
-  ctx.beginPath()
-  ctx.fillStyle = 'rgba(255, 0, 0, 0.05)'
-  ctx.fillRect(area.x, area.y, area.width, area.height)
-}
-
-function getMousePositionOnCanvas(action: MouseEvent) {
-  const canvas = editorCanvas.value
-  const ctx = canvas.getContext('2d')
-
-  const x = action.offsetX
-  const y = action.offsetY
-
-  const transform = ctx.getTransform()
-  const transformedPoint = new DOMPoint(x, y).matrixTransform(transform.inverse())
-
-  return {
-    x: transformedPoint.x,
-    y: transformedPoint.y,
-  }
-}
-
+const selectionArea = new SelectionArea() // annotation box
+let canvasRenderer: CanvasRenderer
 const mouseDownOnCanvas = (action: MouseEvent) => {
   const noController = canvasInteractionState.value
-  const { x, y } = getMousePositionOnCanvas(action)
+  const { x, y } = canvasRenderer.getMousePositionOnCanvas(action)
   mouseCanvasCoordinate.x = x
   mouseCanvasCoordinate.y = y
 
@@ -260,13 +86,13 @@ const mouseWheelOnCanvas = (action: WheelEvent) => {
     return
   }
 
-  action.preventDefault() // prevent the default scrolling behavior
+  action.preventDefault()
 
-  const { x, y } = getMousePositionOnCanvas(action)
+  const { x, y } = canvasRenderer.getMousePositionOnCanvas(action)
   mouseCanvasCoordinate.x = x
   mouseCanvasCoordinate.y = y
 
-  zoom({ x: action.offsetX, y: action.offsetY }, action.deltaY)
+  canvasRenderer.zoom({ x: action.offsetX, y: action.offsetY }, action.deltaY)
 }
 
 const mouseUpOnCanvas = () => {
@@ -285,7 +111,7 @@ const mouseUpOnCanvas = () => {
 }
 
 const mouseMoveOnCanvas = (action: MouseEvent) => {
-  const { x, y } = getMousePositionOnCanvas(action)
+  const { x, y } = canvasRenderer.getMousePositionOnCanvas(action)
   const temp = { x: x, y: y }
 
   if (
@@ -293,18 +119,17 @@ const mouseMoveOnCanvas = (action: MouseEvent) => {
     !canvasInteractionState.value.isResizing &&
     !canvasInteractionState.value.isDrawing
   ) {
-    // check if the user is not dragging or resizing the rectangle
     interactionCursor.value.hovering = selectionArea.contains(temp)
   }
 
   if (canvasInteractionState.value.isPanning) {
-    moveCanvasView(action)
+    canvasRenderer.moveCanvasView(action)
   } else if (canvasInteractionState.value.isDraggable) {
-    movingRectangle(x, y)
+    canvasRenderer.movingRectangle(x, y)
   } else if (canvasInteractionState.value.isResizing) {
-    resizingRectangle(x, y)
+    canvasRenderer.resizingRectangle(x, y)
   } else if (canvasInteractionState.value.isDrawing) {
-    drawOnCanvas(mouseCanvasCoordinate.x, mouseCanvasCoordinate.y, x, y)
+    canvasRenderer.drawOnCanvas(mouseCanvasCoordinate.x, mouseCanvasCoordinate.y, x, y)
   }
 }
 
@@ -319,8 +144,8 @@ const keyUpOnCanvas = (keyCode) => {
       } else {
         ctx.setTransform(new DOMMatrix())
       }
-      canvasBackground()
-      paintIt(selectionArea.x, selectionArea.y, selectionArea.width, selectionArea.height)
+      canvasRenderer.canvasBackground()
+      canvasRenderer.paintIt(selectionArea)
       break
   }
 }
@@ -335,16 +160,22 @@ const keyOnCanvas = (e: MouseEvent) => {
 
 document.addEventListener('keyup', keyOnCanvas)
 
-watch()
-
 onMounted(() => {
   const canvasWidth = 600
   const canvasHeight = 400
   editorCanvasWidth.value = canvasWidth
   editorCanvasHeight.value = canvasHeight
 
+  canvasRenderer = new CanvasRenderer( // canvas space
+    editorCanvas.value,
+    () => videoStore.currentFrame,
+    editorScale.value,
+    panState,
+    selectionArea,
+    interactionCursor.value,
+  )
   // Wait for next animation frame before drawing on canvas or draw calls are dropped
-  window.requestAnimationFrame(canvasBackground)
+  window.requestAnimationFrame(canvasRenderer.canvasBackground)
 })
 </script>
 
